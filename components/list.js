408baaa480069561288ce1e1c7901dfd466a4984
Boppuri <tony.villiams@gmail.com>
2020-05-17 14:04:47 +0100

    Get data from backend

--- components/list.js
+++ components/list.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function MovieList() {
+
+  const [ movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+      fetch('http://10.0.2.2:8000/api/movies/', {
+          method: 'GET',
+          headers: {
+             'Authorization': `Token f5da4e845178ff24b41d39188065699247835def`,
+             'Content-Type': 'application/json'
+          }
+      }).then( res => res.json())
+        .then( jsonRes => setMovies(jsonRes))
+        .catch( error => console.log(error));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>MovieRater!!!</Text>
+      <FlatList
+        data={movies}
+        renderItem={({item}) => (
+            <Text key={item.id}>{item.title}</Text>
+        )}
+        />
     </View>
   );
 }
